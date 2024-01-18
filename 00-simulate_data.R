ebcef135e3a00e4a2aaf4dc02729f6758694e656
yiyifeng <yiyi.feng@mail.utoronto.ca>
2024-01-18 06:02:04 +0000

    1st night work

---
+++ 00-simulate_data.R
@@ -0,0 +1,22 @@
+#### Preamble ####
+# Purpose: Simulate data for 
+# Author: Yiyi Feng
+# Date: 20 January 2024
+# Contact: yiyi.feng@mail.utoronto.ca
+# License: MIT
+# Any other information needed? [...UPDATE THIS...]
+
+
+#### Workspace setup ####
+library(opendatatoronto)
+library(dplyr)
+
+# get package
+package <- show_package("ec53f7b2-769b-4914-91fe-a37ee27a90b3")
+package
+
+#### Simulate data ####
+# [...ADD CODE HERE...]
+
+
+
